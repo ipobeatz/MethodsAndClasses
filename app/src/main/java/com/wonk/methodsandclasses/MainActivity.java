fba23f46d12ebf75a6fdac62f8eed03c5f2370a4
Mrtckr008 <mrtckr008@gmail.com>
2021-07-09 00:54:22 +0300

    * Added print line to onPause function.

--- app/src/main/java/com/wonk/methodsandclasses/MainActivity.java
+++ app/src/main/java/com/wonk/methodsandclasses/MainActivity.java
@@ -77,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onPause() {
+        System.out.println("worked onPause");
         super.onPause();
     }
 }
